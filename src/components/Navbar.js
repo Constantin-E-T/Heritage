4e8a4e18c9b4b30260f588499aa88ed52cd5b89c
Moxy <keanu.knighton@gmail.com>
2023-03-21 21:12:00 +0000

    Navbar layout // no links

---
+++ src/components/Navbar.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    
+    <nav className="navbar navbar-expand-lg navbar-light bg-light row">
+      <a className="navbar-brand col-5" href="#">
+        Logo Img
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavDropdown"
+        aria-controls="navbarNavDropdown"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul className="navbar-nav">
+          <li className="nav-item active">
+            <a className="nav-link" href="#">
+              Home <span className="sr-only">(current)</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              About Us
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Services
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Contact
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Drivers/Team
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+  );
+};
+
+export default Navbar;
