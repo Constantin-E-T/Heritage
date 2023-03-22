85c90c9c320ac7787f9a9920170486f4675196f9
Moxy <keanu.knighton@gmail.com>
2023-03-22 20:52:31 +0000

    Navbar working and rendering page components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Navbar from './components/Navbar';
 import TopHeader from './components/TopHeader';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home'
+import Contact from './components/Contact'
+import About from './components/AboutUs';
+import Services from './components/Services';
+import Team from './components/Team';
 
-function App() {
-  return (
-    <div className="App">
-      <TopHeader />
-      <Navbar />      
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        
+       <div className="App">
+        <TopHeader />
+        <Navbar />
+        <Routes>
+          <Route path="/home" Component={Home} /> 
+          <Route path="/about" Component={About}/>
+          <Route path="/services" Component={Services}/>
+          <Route path="/contact" Component={Contact}/>
+          <Route path="/team" Component={Team}/>
+        </Routes>  
+       </div>
+        
+       </Router>
+    );
+  }
 }
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, {Component} from "react";
+import {Link} from 'react-router-dom';
 
-const Navbar = () => {
+class Navbar extends Component {
+
+  render() {
   return (
     
     <nav className="navbar navbar-expand-lg navbar-light bg-light row">
-      <a className="navbar-brand col-5" href="#">
-        Logo Img
+      <a className="navbar-brand col-5" href="/home">
+      <Link to="/">Logo Img</Link>
       </a>
       <button
         className="navbar-toggler"
@@ -22,27 +25,27 @@ const Navbar = () => {
       <div className="collapse navbar-collapse" id="navbarNavDropdown">
         <ul className="navbar-nav">
           <li className="nav-item active">
-            <a className="nav-link" href="#">
+            <a className="nav-link" href="/home">
               Home <span className="sr-only">(current)</span>
             </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">
+            <a className="nav-link" href="/about">
               About Us
             </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">
+            <a className="nav-link" href="/services">
               Services
             </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">
+            <a className="nav-link" href="/contact">
               Contact
             </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">
+            <a className="nav-link" href="/team">
               Drivers/Team
             </a>
           </li>
@@ -51,6 +54,7 @@ const Navbar = () => {
     </nav>
 
   );
-};
+}
+}
 
 export default Navbar;
