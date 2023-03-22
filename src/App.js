d655b3fcfdc5d9cb40966a055f12402c042c4567
Emilian Con <emiliancon@Emilians-MacBook-Pro-2.local>
2023-03-22 18:58:37 +0000

    done with team component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 // components
 import TopHeader from './components/TopHeader';
+import Team from './components/Team';
 
 function App() {
   return (
     <div>
       <TopHeader />
+      <Team />
     </div>
   );
 }
