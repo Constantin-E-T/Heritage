09ec34c1e96868ebf280102ebe7a085f68b2c5ae
Emilian Con <emiliancon@Emilians-MacBook-Pro-2.local>
2023-03-23 01:54:02 +0000

    fixed a bug with a tag and replace with Link, and added NavLink to make the active link on every clicked link. Also added some bottom shadow on the navbar.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,60 +1,58 @@
-import React, {Component} from "react";
-import {Link} from 'react-router-dom';
+import React, { Component } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import './Navbar.module.css';
 
 class Navbar extends Component {
-
   render() {
-  return (
-    
-    <nav className="navbar navbar-expand-lg navbar-light bg-light row">
-      <Link className="navbar-brand col-5" href="/">
-      Logo Img
-      </Link>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNavDropdown"
-        aria-controls="navbarNavDropdown"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-light row shadow-sm sticky-top">
+        <Link className="navbar-brand col-5" to="/">
+          Logo Img
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-      <div className="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul className="navbar-nav">
-          <li className="nav-item active">
-            <a className="nav-link" href="/home">
-              Home <span className="sr-only">(current)</span>
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/about">
-              About Us
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/services">
-              Services
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/contact">
-              Contact
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/team">
-              Drivers/Team
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-
-  );
-}
+        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink className="nav-link" activeClassName="active" exact to="/home">
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" activeClassName="active" to="/about">
+                About Us
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" activeClassName="active" to="/services">
+                Services
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" activeClassName="active" to="/contact">
+                Contact
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" activeClassName="active" to="/team">
+                Drivers/Team
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
 }
 
 export default Navbar;
