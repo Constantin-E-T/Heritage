957ca2cbefece83c82399288ddc648af236938c5
Moxy <keanu.knighton@gmail.com>
2023-03-23 20:36:17 +0000

    pin implemntation started, not operable

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,8 @@
-import React, { Component} from 'react';
+import React, {Component} from 'react';
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import ReactMapGL, { Marker } from 'mapbox-gl';
 import './Contact.css';
+import pin from '../assets/images/pin.png';
  
 mapboxgl.accessToken = 'pk.eyJ1IjoibW94eS0iLCJhIjoiY2xmbGZ5M2kzMDFsdDN3bGwwazZkZDl1MyJ9.5Ub-_HIlHEOo5kaHtkrJjA';
 
@@ -26,6 +28,13 @@ export default class Contact extends Component {
   });
   }
   render() {
+<ReactMapGL width="100vw" height="100vh">
+  <Marker 
+    longitude= {-1.1013463074087266} 
+    latitude={50.80151276175477} >
+    <img src={pin} />
+  </Marker>
+</ReactMapGL>
     return (
       <div>
         <div ref={this.mapContainer} className="map-container" />
