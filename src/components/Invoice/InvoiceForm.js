b43147862bd21355297587a7b12773a76148d30c
Emilian Con <emiliancon@Emilians-MacBook-Pro-2.local>
2023-03-26 20:22:27 +0100

    invoice component done

--- src/components/Invoice/InvoiceForm.js
+++ src/components/Invoice/InvoiceForm.js
@@ -1,50 +1,61 @@
-import React, { useState } from 'react';
-import { Button, Form, Container, Row, Col } from 'react-bootstrap';
-import InvoicePDF from './InvoicePDF';
-import './InvoiceForm.css';
+import React, { useState } from "react";
+import { Button, Form, Container, Row, Col } from "react-bootstrap";
+import InvoicePDF from "./InvoicePDF";
+import "./InvoiceForm.css";
+import companyInfo from "./companyInfo.json";
 
 const InvoiceForm = () => {
   const [invoiceData, setInvoiceData] = useState(null);
-  const [showPDF, setShowPDF] = useState(false);
+  const [showPDFButtons, setShowPDFButtons] = useState(false);
 
   const [formData, setFormData] = useState({
-    customerName: '',
-    customerEmail: '',
-    invoiceItems: [],
+    customerName: "",
+    customerEmail: "",
+    invoiceNumber: "",
+    orderDate: "",
+    dueDate: "",
+    origin: "",
+    destination: "",
+    weight: "",
+    rate: "",
+    totalAmount: "",
   });
 
   const handleChange = (event) => {
     const { name, value } = event.target;
-    setFormData((prevState) => ({ ...prevState, [name]: value }));
+    setFormData((prevState) => {
+      const updatedState = { ...prevState, [name]: value };
+
+      if (name === "weight" || name === "rate") {
+        const weight = parseFloat(updatedState.weight) || 0;
+        const rate = parseFloat(updatedState.rate) || 0;
+        updatedState.totalAmount = (weight * rate).toFixed(2);
+      }
+
+      return updatedState;
+    });
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
     setInvoiceData(formData);
-    setShowPDF(true);
+    setShowPDFButtons(true);
   };
 
   const handleBack = () => {
-    setFormData({
-      customerName: '',
-      customerEmail: '',
-      invoiceItems: [],
-    });
-    setShowPDF(false);
+    setShowPDFButtons(false);
   };
-
-  if (showPDF) {
-    return <InvoicePDF data={invoiceData} onBack={handleBack} />;
-  }
 
   return (
     <Container className="invoice-form">
       <h1>Create Invoice</h1>
       <Form onSubmit={handleSubmit}>
         <Row>
-          <Col>
+          <Col xs={12} md={6}>
             <Form.Group controlId="customerName">
-              <Form.Label>Customer Name</Form.Label>
+              <Form.Label className="left-align-label">
+                Customer Name
+              </Form.Label>
               <Form.Control
                 type="text"
                 name="customerName"
@@ -54,9 +65,11 @@ const InvoiceForm = () => {
               />
             </Form.Group>
           </Col>
-          <Col>
+          <Col xs={12} md={6}>
             <Form.Group controlId="customerEmail">
-              <Form.Label>Customer Email</Form.Label>
+              <Form.Label className="left-align-label">
+                Customer Email
+              </Form.Label>
               <Form.Control
                 type="email"
                 name="customerEmail"
@@ -67,11 +80,132 @@ const InvoiceForm = () => {
             </Form.Group>
           </Col>
         </Row>
-        {/* Add more form fields as needed */}
-        <Button variant="primary" type="submit">
-          Generate Invoice
-        </Button>
+        <Row>
+          <Col xs={12} md={4}>
+            <Form.Group controlId="invoiceNumber">
+              <Form.Label className="left-align-label">
+                Invoice Number
+              </Form.Label>
+              <Form.Control
+                type="text"
+                name="invoiceNumber"
+                value={formData.invoiceNumber}
+                onChange={handleChange}
+                required
+              />
+            </Form.Group>
+          </Col>
+          <Col xs={12} md={4}>
+            <Form.Group controlId="orderDate">
+              <Form.Label className="left-align-label">Order Date</Form.Label>
+              <Form.Control
+                type="date"
+                name="orderDate"
+                value={formData.orderDate}
+                onChange={handleChange}
+                required
+              />
+            </Form.Group>
+          </Col>
+          <Col xs={12} md={4}>
+            <Form.Group controlId="dueDate">
+              <Form.Label className="left-align-label">Due Date</Form.Label>
+              <Form.Control
+                type="date"
+                name="dueDate"
+                value={formData.dueDate}
+                onChange={handleChange}
+                required
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12} md={6}>
+            <Form.Group controlId="origin">
+              <Form.Label className="left-align-label">Origin</Form.Label>
+              <Form.Control
+                type="text"
+                name="origin"
+                value={formData.origin}
+                onChange={handleChange}
+                required
+              />
+            </Form.Group>
+          </Col>
+          <Col xs={12} md={6}>
+            <Form.Group controlId="destination">
+              <Form.Label className="left-align-label">Destination</Form.Label>
+              <Form.Control
+                type="text"
+                name="destination"
+                value={formData.destination}
+                onChange={handleChange}
+                required
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12} md={6}>
+            <Form.Group controlId="weight">
+              <Form.Label className="left-align-label">Weight (kg)</Form.Label>
+              <Form.Control
+                type="number"
+                name="weight"
+                value={formData.weight}
+                onChange={handleChange}
+                required
+              />
+            </Form.Group>
+          </Col>
+          <Col xs={12} md={6}>
+            <Form.Group controlId="rate">
+              <Form.Label className="left-align-label">Rate (£/kg)</Form.Label>
+              <Form.Control
+                type="number"
+                name="rate"
+                value={formData.rate}
+                onChange={handleChange}
+                required
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12}>
+            <Form.Group controlId="totalAmount">
+              <Form.Label className="left-align-label">
+                Total Amount (£)
+              </Form.Label>
+              <Form.Control
+                type="number"
+                name="totalAmount"
+                value={formData.totalAmount}
+                readOnly
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+
+        {!showPDFButtons && (
+          <Button variant="primary" type="submit" className="my-4">
+            Generate Invoice
+          </Button>
+        )}
       </Form>
+      {showPDFButtons && (
+        <InvoicePDF
+          data={invoiceData}
+          onBack={handleBack}
+          companyInfo={companyInfo}
+          origin={formData.origin}
+          destination={formData.destination}
+          weight={formData.weight}
+          rate={formData.rate}
+          totalAmount={formData.totalAmount}
+        />
+      )}
     </Container>
   );
 };
--- src/components/Invoice/InvoicePDF.js
+++ src/components/Invoice/InvoicePDF.js
@@ -1,27 +1,71 @@
-import React from 'react';
-import { jsPDF } from 'jspdf';
-import 'jspdf-autotable';
-import { Button } from 'react-bootstrap';
+import React from "react";
+import { jsPDF } from "jspdf";
+import "jspdf-autotable";
+import { Button } from "react-bootstrap";
 
-const InvoicePDF = ({ data, onBack }) => {
+const InvoicePDF = ({
+  data,
+  onBack,
+  companyInfo,
+  origin,
+  destination,
+  weight,
+  rate,
+  totalAmount,
+}) => {
   const generatePDF = () => {
     const doc = new jsPDF();
 
-    // Add the invoice content to the PDF using 'doc' methods
-    // Use doc.autoTable() to generate tables from the data
+    // Add the company information
+    doc.setFontSize(18);
+    doc.text(companyInfo.name, 10, 20);
+    doc.setFontSize(12);
+    doc.text(companyInfo.addressLine1, 10, 30);
+    doc.text(companyInfo.addressLine2, 10, 35);
+    doc.text(companyInfo.addressLine3, 10, 40);
+    doc.text("Phone: " + companyInfo.phone, 10, 45);
 
-    doc.save('invoice.pdf');
+    // Add the invoice information
+    doc.text("Invoice No: " + data.invoiceNumber, 150, 20);
+    doc.text("Invoice Date: " + data.orderDate, 150, 25);
+    doc.text("Due Date: " + data.dueDate, 150, 30);
+
+    // Add the customer information
+    doc.text("Bill To:", 10, 60);
+    doc.text(data.customerName, 10, 65);
+    doc.text(data.customerEmail, 10, 70);
+
+    // Create the table for invoice items
+    const invoiceItems = [
+      { item: "Origin", value: origin },
+      { item: "Destination", value: destination },
+      { item: "Weight (kg)", value: weight },
+      { item: "Rate (£/kg)", value: rate },
+      { item: "Total Amount (£)", value: totalAmount },
+    ];
+
+    doc.autoTable({
+      startY: 90,
+      head: [["Item", "Value"]],
+      body: invoiceItems.map((item) => [item.item, item.value]),
+      theme: "grid",
+    });
+
+    // Save the PDF
+    doc.save("invoice.pdf");
   };
+
+  if (!data) {
+    return null;
+  }
 
   return (
     <div className="invoice-pdf">
-      <h1>Invoice Preview</h1>
-      {/* Display the invoice data in a preview format */}
-      <Button variant="success" onClick={generatePDF}>
+      <Button variant="success" onClick={generatePDF} className="my-4">
         Download Invoice
       </Button>
-      <Button variant="secondary" onClick={onBack} className="ml-2">
-        Back to Form
+      <Button variant="secondary" onClick={onBack} className="ml-2 my-4">
+        Clear the Form
       </Button>
     </div>
   );
