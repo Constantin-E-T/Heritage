9e3b6896cf00b24c71c3d30e39c4b0b6e641dc20
Moxy <keanu.knighton@gmail.com>
2023-03-26 17:16:14 +0100

    contact page map changed to static img with pin marker

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,44 +1,17 @@
 import React, {Component} from 'react';
-import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
-import ReactMapGL, { Marker } from 'mapbox-gl';
 import './Contact.css';
-import pin from '../assets/images/pin.png';
+import locationStatic from '../assets/images/locationStatic.jpg';
  
-mapboxgl.accessToken = 'pk.eyJ1IjoibW94eS0iLCJhIjoiY2xmbGZ5M2kzMDFsdDN3bGwwazZkZDl1MyJ9.5Ub-_HIlHEOo5kaHtkrJjA';
 
 export default class Contact extends Component {
-  constructor(props) 
- {
-  super(props)
-  this.state = {
-    lng: -1.1013463074087266,
-    lat: 50.80151276175477,
-    zoom: 14
-   };
-   this.mapContainer = React.createRef();
-  }
-  componentDidMount() 
- {
-  const { lng, lat, zoom } = this.state;
-  const map = new mapboxgl.Map({
-    container: this.mapContainer.current,
-    style: 'mapbox://styles/mapbox/streets-v12',
-    center: [lng, lat],
-    zoom: zoom
-  });
-  }
   render() {
-<ReactMapGL width="100vw" height="100vh">
-  <Marker 
-    longitude= {-1.1013463074087266} 
-    latitude={50.80151276175477} >
-    <img src={pin} />
-  </Marker>
-</ReactMapGL>
     return (
       <div class='contact-container row'>
-        <div ref={this.mapContainer} className="map-container col-lg-8" />
-          <form class='contact col-lg-4' name="contact" method="POST" data-netlify="true" data-netlify-recaptcha="true">
+        <img  class='col-md-7'
+              style={{height:'400px', width:'800px', paddingRight:'50px'}}
+              alt='Heritage Haulage Static Map Location'
+              src={locationStatic}/>
+          <form class='contact col-lg-5' name="contact" method="POST" data-netlify="true" data-netlify-recaptcha="true">
               <div class="form-group">
                 <label for="email">Email:</label>
                 <input type="email" class="form-control" id="email" name="email" required/>
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import ReactMapGL, { Marker } from 'mapbox-gl';
+// import './Services.css';
+
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibW94eS0iLCJhIjoiY2xmbGZ5M2kzMDFsdDN3bGwwazZkZDl1MyJ9.5Ub-_HIlHEOo5kaHtkrJjA';
 
 class Services extends Component {
+  
   render() {
     return (
       <div>
