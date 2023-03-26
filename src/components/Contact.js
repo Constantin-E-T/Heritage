c9299be6906c30db70399b0078aabd05aa9a0c02
Emilian Con <emiliancon@Emilians-MacBook-Pro-2.local>
2023-03-26 20:37:41 +0100

    added eslint-disable-next-line no-unused-vars for the const map line 21

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,35 +1,36 @@
-// import React, { Component} from 'react';
-// import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
-// import './Contact.css';
+import React, { Component} from 'react';
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import './Contact.css';
  
-// mapboxgl.accessToken = 'pk.eyJ1IjoibW94eS0iLCJhIjoiY2xmbGZ5M2kzMDFsdDN3bGwwazZkZDl1MyJ9.5Ub-_HIlHEOo5kaHtkrJjA';
+mapboxgl.accessToken = 'pk.eyJ1IjoibW94eS0iLCJhIjoiY2xmbGZ5M2kzMDFsdDN3bGwwazZkZDl1MyJ9.5Ub-_HIlHEOo5kaHtkrJjA';
 
-// export default class Contact extends Component {
-//   constructor(props) 
-//  {
-//   super(props)
-//   this.state = {
-//     lng: -1.1013463074087266,
-//     lat: 50.80151276175477,
-//     zoom: 16
-//    };
-//    this.mapContainer = React.createRef();
-//   }
-//   componentDidMount() 
-//  {
-//   const { lng, lat, zoom } = this.state;
-//   const map = new mapboxgl.Map({
-//     container: this.mapContainer.current,
-//     style: 'mapbox://styles/mapbox/streets-v12',
-//     center: [lng, lat],
-//     zoom: zoom
-//   });
-//   }
-//   render() {
-//     return (
-//       <div>
-//         <div ref={this.mapContainer} className="map-container" />
-//       </div>
-//     );
-//   }
-// }
+export default class Contact extends Component {
+  constructor(props) 
+ {
+  super(props)
+  this.state = {
+    lng: -1.1013463074087266,
+    lat: 50.80151276175477,
+    zoom: 16
+   };
+   this.mapContainer = React.createRef();
+  }
+  componentDidMount() 
+ {
+  const { lng, lat, zoom } = this.state;
+  // eslint-disable-next-line no-unused-vars
+  const map = new mapboxgl.Map({
+    container: this.mapContainer.current,
+    style: 'mapbox://styles/mapbox/streets-v12',
+    center: [lng, lat],
+    zoom: zoom
+  });
+  }
+  render() {
+    return (
+      <div>
+        <div ref={this.mapContainer} className="map-container" />
+      </div>
+    );
+  }
+}
