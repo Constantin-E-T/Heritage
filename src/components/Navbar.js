a53c3297cd4727f3df46a880f75d439411fe4487
Moxy <keanu.knighton@gmail.com>
2023-03-26 14:18:07 +0100

    Navbar styled

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,9 @@ import heritageLogo from '../assets/images/heritageLogo.png';
 class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light row shadow-sm sticky-top">
+      <nav className="navbar navbar-expand-lg navbar-light row shadow-sm sticky-top" 
+      style={{backgroundColor:'#094912'}}>
+
         <NavLink className="navbar-brand col-6" to="/">
           <img style={{width: '350px', height: '120px'}} src={heritageLogo}/>
         </NavLink>
@@ -26,22 +28,22 @@ class Navbar extends Component {
         <div className="collapse navbar-collapse" id="navbarNavDropdown">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <NavLink className="nav-link" to="/about">
+              <NavLink className="nav-link" to="/about" style={{color:'#d4b080', fontSize:'large'}}>
                 About Us
               </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to="/services">
+              <NavLink className="nav-link" to="/services" style={{color:'#d4b080', fontSize:'large'}}>
                 Services
               </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to="/contact">
+              <NavLink className="nav-link" to="/contact" style={{color:'#d4b080', fontSize:'large'}}>
                 Contact
               </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to="/team">
+              <NavLink className="nav-link" to="/team" style={{color:'#d4b080', fontSize:'large'}}>
                 Drivers/Team
               </NavLink>
             </li>
