b46b9782fc6a1a5682b30183b44af6498e45ad22
Emilian Con <emiliancon@Emilians-MacBook-Pro-2.local>
2023-03-28 23:38:59 +0100

    new updates on the UI/UX of the TopHeader component

--- src/components/TopHeader.js
+++ src/components/TopHeader.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import WeatherWidget from './WeatherWidget';
+import { Container, Row, Col } from "react-bootstrap";
+import "./TopHeader.css"
+
 
 const TopHeader = () => {
   return (
-    <div className="navbar-custom text-white py-2">
-      <div className="container">
-        <div className="d-flex justify-content-between align-items-center">
-          <WeatherWidget />
-          <div>
-            <i className="fas fa-phone mr-1"></i>
-            <a href="tel:+1234567890" className="text-white">0808 188 9006</a>
-            <span className="mx-3">|</span>
-            <i className="fas fa-envelope mr-1"></i>
-            <a href="mailto:info@example.com" className="text-white">info@example.com</a>
-          </div>
-        </div>
-      </div>
-    </div>
+<div className="navbar-custom-light text-white">
+  <Container fluid>
+    <Row className="align-items-center">
+      <Col xs={12} md={6} className="text-md-left ">
+        <WeatherWidget />
+      </Col>
+      <Col xs={12} md={6} className="my-2 text-md-right">
+        <i className="fas fa-phone mr-1"></i>
+        <a href="tel:+1234567890" className="text-white">0808 188 9006</a>
+        <span className="mx-3">|</span>
+        <i className="fas fa-envelope mr-1"></i>
+        <a href="mailto:info@example.com" className="text-white">info@example.com</a>
+      </Col>
+    </Row>
+  </Container>
+</div>
+
+
   );
 };
 
