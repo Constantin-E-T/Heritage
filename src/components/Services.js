43d8746d275ae8f36931c192e499bd2f23330d58
Emilian Con <emiliancon@Emilians-MacBook-Pro-2.local>
2023-03-29 08:34:41 +0100

    done with the Services component. added nice animated effect on elements using AOS package

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,98 +1,115 @@
 import React, { Component } from "react";
 import Stockbanner from "../assets/images/Stockbanner.jpg";
 import "../assets/css/Services.css";
-import 'font-awesome/css/font-awesome.min.css';
-
+import "font-awesome/css/font-awesome.min.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 class Services extends Component {
+  componentDidMount() {
+    AOS.init({
+      duration: 1500,
+    });
+  }
+
   render() {
     return (
       <div className="container text-justify">
-        <div className="container my-5">
+        <div className="container my-5" data-aos="fade-up">
           <h4 className="heading-line-height">Services</h4>
 
           <div className="row" style={{ marginTop: "2rem" }}>
-            <div className="col-12 col-md-6">
-              <div className="d-flex flex-column mt-2">
-                <a
-                  className="btn btn-primary btn-block mb-4"
-                  href="/invoice"
-                  role="button"
-                >
-                  Create Invoice
-                </a>
-                <a
-                  className="btn btn-primary btn-block mb-4"
-                  href="/contact"
-                  role="button"
-                >
-                  Request Quote
-                </a>
-              </div>
-            </div>
-            <div className="col-12 col-md-6 text-center text-md-left">
-              Our modern fleet ensures reliable and efficient services, ranging
-              from single pallet deliveries to full load shipments. Delivering
-              to a wide variety of locations and working with several major
-              ports we can guarantee you our service anywhere in the UK.
-            </div>
+        <div className="col-12 col-md-6" data-aos="fade-right">
+          <div className="d-flex flex-column mt-2">
+            <a
+              className="btn btn-primary btn-block mb-4"
+              href="/invoice"
+              role="button"
+            >
+              Create Invoice
+            </a>
+            <a
+              className="btn btn-primary btn-block mb-4"
+              href="/contact"
+              role="button"
+            >
+              Request Quote
+            </a>
           </div>
         </div>
-
-        <hr></hr>
-        <div className="row">
-          <div className="col-md-4">
-            <ul style={{ listStyleType: "none" }}>
-              <li>
-                <h6 className="heading-line-height">Services include:</h6>
-              </li>
-
-              <li>Container Transport </li>
-              <li>Tautliners</li>
-              <li>Refrigerated Shipping</li>
-              <li>Full load shipments</li>
-              <li>Groupage deliveries</li>
-              <li>Single pallet deliveries</li>
-            </ul>
-          </div>
-          <div className="col-md-4">
-            <ul style={{ listStyleType: "none" }}>
-              <li>
-                <h6 className="heading-line-height">
-                  Some of the major areas we cover:
-                </h6>
-              </li>
-
-              <li>London</li>
-              <li>Manchester</li>
-              <li>Birmingham</li>
-              <li>Glasgow</li>
-              <li>Leeds</li>
-              <li>Liverpool</li>
-            </ul>
-          </div>
-          <div className="col-md-4">
-            <ul style={{ listStyleType: "none" }}>
-              <li>
-                <h6 className="heading-line-height">Major Port locations:</h6>
-              </li>
-
-              <li>Port of Southampton</li>
-              <li>Port of Felixstowe</li>
-              <li>Port of London Gateway</li>
-              <li>Port of Liverpool</li>
-              <li>Port of Bristol</li>
-            </ul>
-          </div>
+        <div
+          className="col-12 col-md-6 text-center text-md-left"
+          data-aos="fade-left"
+        >
+          Our modern fleet ensures reliable and efficient services, ranging
+          from single pallet deliveries to full load shipments. Delivering
+          to a wide variety of locations and working with several major
+          ports we can guarantee you our service anywhere in the UK.
         </div>
-        <img
-          src={Stockbanner}
-          className="img-fluid mb-5"
-          alt="responsive banner img"
-        />
       </div>
-    );
-  }
+    </div>
+
+    <hr data-aos="fade-up" data-aos-delay="100"></hr>
+    <div className="row" data-aos="fade-up" data-aos-delay="200">
+      <div className="col-xl-4 p-0">
+        <ul style={{ listStyleType: "none" }}>
+          <li>
+            <h6 className="heading-line-height heading-with-icon">
+              <i className="fas fa-truck icon"></i> Services include:
+            </h6>
+          </li>
+
+          <li>Container Transport </li>
+          <li>Tautliners</li>
+          <li>Refrigerated Shipping</li>
+          <li>Full load shipments</li>
+          <li>Groupage deliveries</li>
+          <li>Single pallet deliveries</li>
+        </ul>
+      </div>
+      <div className="col-xl-4 p-0">
+        <ul style={{ listStyleType: "none" }}>
+          <li>
+            <h6 className="heading-line-height heading-with-icon">
+              <i className="fas fa-map-marker-alt icon"></i> Areas we cover:
+            </h6>
+          </li>
+
+          <li>London</li>
+          <li>Manchester</li>
+          <li>Birmingham</li>
+          <li>Glasgow</li>
+          <li>Leeds</li>
+          <li>Liverpool</li>
+        </ul>
+      </div>
+      <div className="col-xl-4 p-0">
+        <ul style={{ listStyleType: "none" }}>
+          <li>
+            <h6 className="heading-line-height heading-with-icon">
+              <i className="fas fa-ship icon"></i> Major Port locations:
+            </h6>
+          </li>
+
+          <li>Port of Southampton</li>
+          <li>Port of Felixstowe</li>
+          <li>Port of London Gateway</li>
+          <li>Port of Liverpool</li>
+          <li>Port of Bristol</li>
+        </ul>
+      </div>
+    </div>
+    <img
+      src={Stockbanner}
+      className="img-fluid mb-5"
+      alt="responsive banner img"
+      data-aos="zoom-in"
+    />
+  </div>
+);
+
+}
 }
 
 export default Services;
+
